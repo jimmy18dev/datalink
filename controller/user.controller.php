ed548cf98c0b3d9abbddfd6a363c88550528ff21
mrjimmy18@gmail.com <mrjimmy18@gmail.com>
2016-05-18 17:10:11 +0700

    alpha 3.8
    - Code cleaning : user controller, model, template

--- controller/user.controller.php
+++ controller/user.controller.php
@@ -13,24 +13,6 @@ class UserController extends UserModel{
 	public $type;
 	public $status;
 
-	// Register new user
-	public function register($section_id,$code,$fname,$lname,$username,$password,$line_default){
-		$already_id = parent::already($code,$fname,$lname);
-
-		if(empty($already_id)){
-			return parent::create($section_id,$code,$fname,$lname,$username,$password,$line_default);
-		}else{
-			return 0;
-		}
-	}
-
-	public function editInfo($id,$section_id,$code,$fname,$lname,$username,$password,$line_default){
-
-		if(empty($id) || empty($code) || empty($fname) || empty($lname) || empty($password)){ return false; }
-		
-		parent::edit($id,$section_id,$code,$fname,$lname,$username,$password,$line_default);
-	}
-
 	public function login($username,$password){
 		if(empty($username) || empty($password)){ return false; }
 
@@ -64,50 +46,18 @@ class UserController extends UserModel{
 		$dataset = parent::getData($id);
 		parent::updateVisitTime($id);
 
-		$this->id = $dataset['id'];
-		$this->code = $dataset['code'];
-		$this->fname = $dataset['fname'];
-		$this->lname = $dataset['lname'];
-		$this->name = $this->fname.' '.$this->lname;
-		$this->password = $dataset['password'];
-		$this->line_default = $dataset['line_default'];
-		$this->register_time = $dataset['register_time'];
-		$this->update_time = $dataset['update_time'];
-		$this->visit_time = $dataset['visit_time'];
-		$this->type = $dataset['type'];
-		$this->status = $dataset['status'];
+		$this->id 				= $dataset['id'];
+		$this->code 			= $dataset['code'];
+		$this->fname 			= $dataset['fname'];
+		$this->lname 			= $dataset['lname'];
+		$this->name 			= $this->fname.' '.$this->lname;
+		$this->password 		= $dataset['password'];
+		$this->line_default 	= $dataset['line_default'];
+		$this->register_time 	= $dataset['register_time'];
+		$this->update_time 		= $dataset['update_time'];
+		$this->visit_time 		= $dataset['visit_time'];
+		$this->type 			= $dataset['type'];
+		$this->status 			= $dataset['status'];
 	}
-
-	public function listAllUser($option){
-		$data = parent::listall();
-		$this->render($data,$option);
-	}
-
-	// render dataset to view.
-    private function render($data,$option){
-    	$total_items = 0;
-        if($option['type'] == 'user-items'){
-            foreach ($data as $var){
-
-            	// Online checking
-            	$timestamp = strtotime($var['visit']);
-            	if(time() - $timestamp < 300){
-            		$online = true;
-            	}else{
-            		$online = false;
-            	}
-
-                include'template/user/user.items.php';
-                $total_items++;
-            }
-
-            if($total_items == 0){
-            	// include'template/article/article.empty.items.php';
-            }
-        }
-
-        unset($data);
-        $total_items = 0;
-    }
 }
 ?>
