e222a4ae351e8451c6dae02a51eb086be9e1d335
mrjimmy18@gmail.com <mrjimmy18@gmail.com>
2016-06-22 15:28:48 +0700

    alpha 9.7

--- movement/template/caliber/caliber.choose.items.php
+++ movement/template/caliber/caliber.choose.items.php
@@ -1,13 +1,10 @@
-<?php if(empty($var['detail_id'])){?>
 <a href="report_detail_editor.php?caliber=<?php echo $var['caliber_id'];?>&header=<?php echo $header_id;?>&action=create" class="caliber-choose-items">
 	<span class="title"><?php echo $var['caliber_code'].' '.$var['caliber_family'];?></span>
 	<span class="std">Standard time <?php echo $var['caliber_stdtime'];?> Hrs/K and has <?php echo $var['total_operation'];?> operations</span>
+
+	<?php if(empty($var['detail_id'])){?>
 	<span class="icon"><i class="fa fa-angle-right" aria-hidden="true"></i></span>
-</a>
-<?php }else{?>
-<div class="caliber-choose-items unlink-items">
-	<span class="title"><?php echo $var['caliber_code'].' '.$var['caliber_family'];?></span>
-	<span class="std">Standard time <?php echo $var['caliber_stdtime'];?> Hrs/K and has <?php echo $var['total_operation'];?> operations</span>
+	<?php }else{?>
 	<span class="icon"><i class="fa fa-check" aria-hidden="true"></i></span>
-</div>
-<?php }?>
+	<?php }?>
+</a>
