3589885f1f6a9eb400f9e4adf8b708bd1a2549ab
mrjimmy18@gmail.com <mrjimmy18@gmail.com>
2017-07-10 17:46:53 +0700

    0.34

--- movement/navigator.php
+++ movement/navigator.php
@@ -4,5 +4,5 @@
 	<a href="message.php" class="header-items <?php echo ($current_page['1'] == 'message'?'header-items-active':'');?>">NEWS(<?php echo $message->countMessage();?>)<span class="tri"></span></a>
 	<a href="profile.php" class="header-items <?php echo ($current_page['1'] == 'profile'?'header-items-active':'');?>"><?php echo $user->fname?><span class="tri"></span></a>
 
-	<a href="../logout.php" class="header-items logout">LOGOUT</a>
+	<!-- <a href="../logout.php" class="header-items logout">LOGOUT</a> -->
 </nav>
