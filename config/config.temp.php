0ac81cea0c7b833a21e6d72cc281684d5f576079
mrjimmy18@gmail.com <mrjimmy18@gmail.com>
2017-10-24 12:06:04 +0700

    Add config file.

---
+++ config/config.temp.php
@@ -0,0 +1,7 @@
+<?php
+// Config database connection and change file name to config.php
+define("DB_HOST", "localhost");
+define("DB_USER", "xxxx");
+define("DB_PASS", "xxxx");
+define("DB_NAME", "xxxx");
+?>
